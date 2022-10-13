874bb4ba622f388e45d084ea9d10fe2fd630aed4
franklinnazareno <franklinnazareno@gmail.com>
2022-10-13 22:27:50 +0800

    Fixed Error message

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,8 +1,92 @@
+import { Typography, Container, Button, makeStyles, TextField, FormControl, FormLabel } from "@mui/material";
+import { useState } from "react";
+
+const classes = {
+    field: {
+        marginTop: 2,
+        marginBottom: 2,
+        display: 'block'
+    }
+}
+
 const Profile = () => {
+    const [email, setEmail] = useState('');
+    const [role, setRole] = useState('');
+    const [password, setPassword] = useState ('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+    const handleSubmit = (e) => {
+        setPasswordError(false)
+        setConfirmPasswordError(false)
+
+        if (password === '') {
+            setPasswordError(true)
+        }
+        if (password === confirmPassword) {
+            console.log("change password success")
+        }
+        if (password != confirmPassword){
+            console.log("change password error")
+            setConfirmPasswordError(true)
+        }
+        setPassword("")
+        setConfirmPassword("")
+    }
     return ( 
-        <div>
-            Profile
-        </div>
+        <Container>
+            <Typography
+             variant="h6" 
+             color="primary">
+                Profile
+            </Typography>
+            <TextField
+                sx={classes.field}
+                label="Email"
+                variant="outlined"
+                color="secondary"
+                disabled
+                helperText="You cannot change your email"
+            />
+            <TextField
+                sx={classes.field}
+                label="Role"
+                variant="outlined"
+                color="secondary"
+                disabled
+            />
+            <FormControl>
+                <FormLabel>Change password</FormLabel>
+                    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                        <TextField
+                            sx={classes.field}
+                            onChange={(e) => setPassword(e.target.value)}
+                            label="Password"
+                            variant="outlined"
+                            color="secondary"
+                            required
+                            error={passwordError}
+                        />
+                        <TextField
+                            sx={classes.field}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            label="Confirm Password"
+                            variant="outlined"
+                            color="secondary"
+                            required
+                            error={confirmPasswordError}
+                        />
+                        <Button 
+                            variant="contained" 
+                            color="primary"
+                            type="submit"
+                            >
+                            Change Password
+                        </Button>
+                    </form>
+            </FormControl>
+        </Container>
      );
 }
  
