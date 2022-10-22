c0c93e87e075109a6aef9635562a4852d79f5594
franklinnazareno <franklinnazareno@gmail.com>
2022-10-22 09:31:46 +0800

    Added more error checking

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -37,6 +37,14 @@ router.post('/', [ auth, [
     
     const { courses, facebook, linkedin } = req.body
 
+    if (!facebook.includes("facebook.com")) {
+        return res.status(400).json({ error: "Please use a Facebook link" })
+    }
+
+    if (!linkedin.includes("linkedin.com")) {
+        return res.status(400).json({ error: "Please use a LinkedIn link" })
+    }
+
     // Build profile object
     const profileFields = {}
     profileFields.user = req.user.id
