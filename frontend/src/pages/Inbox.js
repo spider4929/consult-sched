02f075caf110c23f59e8e24e43f3866dbaf52c0c
tryst6969 <trystanretirado@gmail.com>
2022-10-26 21:24:47 +0800

    c

--- frontend/src/pages/Inbox.js
+++ frontend/src/pages/Inbox.js
@@ -1,9 +1,60 @@
-const Inbox = () => {
-    return ( 
-        <div>
-            Inbox
+import React, { Component } from 'react'
+import { renderMatches } from 'react-router-dom'
+import "./inbox.css";
+
+export default class Inbox extends Component {
+    render() {
+        return (
+        
+        <div className="container">
+            <form>
+                {/*Start*/}
+                <div className="singleItem">
+                    <label htmlFor="name">Firstname</label>
+                    <input type="text"
+                     name="name"
+                     className="name"
+                     placeholder="Firstame..."></input>
+                </div>
+                {/*end*/}
+
+                {/*Start*/}
+                <div className="singleItem">
+                    <label htmlFor="lastname">Lastname</label>
+                    <input type="text"
+                     name="lastname"
+                     className="lastname"
+                     placeholder="Lastname..."></input>
+                </div>
+                {/*end*/}
+
+                {/*Start*/}
+                <div className="singleItem">
+                    <label htmlFor="email">Email</label>
+                    <input type="text"
+                     name="email"
+                     className="name"
+                     placeholder="Email..."></input>
+                </div>
+                {/*end*/}
+
+                {/*Start*/}
+                <div className="teaxtAreasingleItem">
+                <label htmlFor="message">Message</label>
+                    <textarea name ="" 
+                    id="" 
+                    cols="30" 
+                    rows = "5"
+                    placeholder="Message..."></textarea>
+                </div>
+                {/*end*/}
+                <div className="msg"></div>
+                <div className="btn">
+                    <button type="submit" >Submit</button>
+                </div>
+            </form>
+    
         </div>
-     );
+        )
+    }
 }
- 
-export default Inbox;
