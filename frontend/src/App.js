4cd566e7fbd4a5cb05dac5ee0e025f6319f18c61
Kristian Baturiano <kjqb4929@gmail.com>
2022-10-27 21:56:56 +0800

    Random changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Inbox from './pages/Inbox'
 import Layout from './components/Layout'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Test from './pages/Test'
 
 const theme = createTheme({
   palette: {
@@ -35,38 +36,42 @@ function App() {
       <BrowserRouter>
           <Layout>
             <Routes>
-              <Route 
-                path="/profile" 
-                element={ user ? <Profile /> : <Navigate to="/login"/> } 
-              />
-              <Route 
-                path="/" 
-                element={ user ? <Dashboard /> : <Navigate to="/login"/> }
-              />
-              <Route 
-                path="/view-consultations" 
-                element={ user ? <ViewConsul /> : <Navigate to="/login"/> }
-              />
-              <Route 
-                path="/create-consultation-student" 
-                element={ user && user.role === 1 ? <CreateConsulStudent /> : <Navigate to="/login"/> } 
-              />
-              <Route 
-                path="/approve-consultation" 
-                element={ user && user.role === 2 ? <ApproveConsulInstructor /> : <Navigate to="/login"/> }
-              />
-              <Route 
-                path="/inbox" 
-                element={ user ? <Inbox /> : <Navigate to="/login"/> }
-              />
-              <Route 
-                path="/register" 
-                element={ !user ? <Register /> : <Navigate to="/"/> }
-              />
                 <Route 
-                path="/login" 
-                element={ !user ? <Login /> : <Navigate to="/"/> }
-              />
+                    path="/profile" 
+                    element={ user ? <Profile /> : <Navigate to="/login"/> } 
+                />
+                <Route 
+                    path="/" 
+                    element={ user ? <Dashboard /> : <Navigate to="/login"/> }
+                />
+                <Route 
+                    path="/view-consultations" 
+                    element={ user ? <ViewConsul /> : <Navigate to="/login"/> }
+                />
+                <Route 
+                    path="/create-consultation-student" 
+                    element={ user && user.role === 1 ? <CreateConsulStudent /> : <Navigate to="/login"/> } 
+                />
+                <Route 
+                    path="/approve-consultation" 
+                    element={ user && user.role === 2 ? <ApproveConsulInstructor /> : <Navigate to="/login"/> }
+                />
+                <Route 
+                    path="/inbox" 
+                    element={ user ? <Inbox /> : <Navigate to="/login"/> }
+                />
+                <Route 
+                    path="/register" 
+                    element={ !user ? <Register /> : <Navigate to="/"/> }
+                />
+                <Route 
+                    path="/login" 
+                    element={ !user ? <Login /> : <Navigate to="/"/> }
+                />
+                <Route 
+                    path="/test" 
+                    element= {<Test />}
+                />
             </Routes>
           </Layout>
       </BrowserRouter>
