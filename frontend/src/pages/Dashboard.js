d7959e7b41099f96114704a7518ccfe53116cb4c
tryst6969 <trystanretirado@gmail.com>
2022-11-05 00:41:53 +0800

    It is now clickable

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,49 @@
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import Badge from '@mui/material/Badge';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import CheckIcon from '@mui/icons-material/Check';
+import './Dashboard.css'
 
 const Dashboard = () => {
 
-    return ( 
-        <div>
-            <Typography>Dashboard in progress...</Typography>
-        </div>
-     );
-}
- 
+    const [value, setValue] = useState(new Date());
+  const [highlightedDays, setHighlightedDays] = useState([1, 2, 3]);//dont forget this 
+  return (
+    
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <div className='Header'>Dashboard</div>
+      <StaticDatePicker
+        // mask='____/__/__'
+        variant='static'
+        orientation='portrait'
+        value={value}
+        
+        onChange={(newValue) => setValue(newValue)}
+        renderInput={(params) => {
+          <TextField {...params} />;
+        }}
+        renderDay={(day, _value, DayComponentProps) => {
+          const isSelected =
+            !DayComponentProps.outsideCurrentMonth &&
+            highlightedDays.indexOf(day.getDate()) >= 0;
+
+          return (
+            <Badge
+              key={day.toString()}
+              overlap='circular'
+              badgeContent={isSelected ? <CheckIcon color='red' /> : undefined}
+            >
+              <PickersDay {...DayComponentProps} />
+            </Badge>
+          );
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
+
 export default Dashboard;
