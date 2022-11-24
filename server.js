d0d1a793519fdf4c4fb9fecbfae60ef2d5a1c337
franklinnazareno <franklinnazareno@gmail.com>
2022-11-24 22:26:21 +0800

    Revert

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use('/api/chat', require('./routes/api/chat'))
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, 'frontend', 'build')))
+    app.use(express.static('frontend/build'))
 
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
