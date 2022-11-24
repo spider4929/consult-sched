00e723c63333e7a47ace01f349a52532cc71e6e2
franklinnazareno <franklinnazareno@gmail.com>
2022-11-25 00:49:26 +0800

    added cors v2

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path')
 
 const app = express()
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 // Connect Database
 connectDB()
 
