58fcc6d0b73d76036356f882878edbe103ed7f72
franklinnazareno <franklinnazareno@gmail.com>
2022-11-29 18:57:17 +0800

    Work or imma

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT, () => console.log(`Server started on port ${PORT
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://frontend-production-35f9.up.railway.app",
     }
 })
 
